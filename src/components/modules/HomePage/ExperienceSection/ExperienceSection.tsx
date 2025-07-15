5c55a2cf960e3cb22b4ac328be2c94c65c2b4e94
fayshal-bin-amir-002 <foyshalbinamir@gmail.com>
2025-07-16 02:07:42 +0600

    added: testimonial add modal

--- src/components/modules/HomePage/ExperienceSection/ExperienceSection.tsx
+++ src/components/modules/HomePage/ExperienceSection/ExperienceSection.tsx
@@ -4,8 +4,33 @@ import { TestimonialsSection } from "@/components/blocks/testimonials-with-marqu
 import SectionContainer from "@/components/shared/SectionContainer";
 import { testimonials } from "@/constants";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Textarea } from "@/components/ui/textarea";
 
 export const ExperienceSection = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <SectionContainer>
       <h2 className="text-3xl md:text-4xl font-bold text-center mb-10">
@@ -32,11 +57,145 @@ export const ExperienceSection = () => {
               </p>
             </div>
             <div className="mt-6">
-              <Button className="w-full">✍️ অভিজ্ঞতা যুক্ত করুন</Button>
+              <TestimonialAddModal />
             </div>
           </div>
         </div>
       </div>
     </SectionContainer>
+  );
+};
+
+const formSchema = z.object({
+  name: z.string().trim().min(1, "Name is Required"),
+  address: z.string().trim().min(1, "Address is Required"),
+  message: z
+    .string()
+    .min(30, "Message must be at least 30 character long")
+    .max(200, "Message could not be more than 200 character"),
+});
+
+const TestimonialAddModal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      address: "",
+      message: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      console.log(values);
+      setTimeout(() => {
+        setIsOpen(false);
+        form.reset();
+      }, 2000);
+      toast(
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      );
+    } catch (error) {
+      console.error("Form submission error", error);
+      toast.error("Failed to submit the form. Please try again.");
+    }
+  }
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button className="w-full" onClick={() => setIsOpen(true)}>
+          ✍️ অভিজ্ঞতা যুক্ত করুন
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-h-[85vh] rounded-lg overflow-y-auto">
+        <DialogHeader>
+          <DialogTitle>আপনার গল্প শেয়ার করুন</DialogTitle>
+          <DialogDescription>
+            আপনি কিভাবে রক্ত দিয়েছেন বা পেয়েছেন — সেই অভিজ্ঞতা আমাদের জানাতে
+            পারেন।
+          </DialogDescription>
+        </DialogHeader>
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-8 max-w-3xl mx-auto py-10"
+            >
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Type your name.."
+                        type="text"
+                        {...field}
+                        value={field.value || ""}
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="address"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Address</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Type your address..."
+                        type="text"
+                        {...field}
+                        value={field.value || ""}
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="message"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Message</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="Type your message"
+                        className="resize-none"
+                        {...field}
+                        value={field.value || ""}
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-end items-center gap-4 pt-4">
+                <DialogClose asChild>
+                  <Button type="button" variant="outline">
+                    Cancel
+                  </Button>
+                </DialogClose>
+                <Button type="submit">Submit</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 };
