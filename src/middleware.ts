ce68d3c95fef8894f5e459241c94672d6969f33b
fayshal-bin-amir-002 <foyshalbinamir@gmail.com>
2025-07-24 16:44:17 +0600

    updated: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,7 +21,7 @@ export const middleware = async (request: NextRequest) => {
     } else {
       return NextResponse.redirect(
         new URL(
-          `http://localhost:3000/auth?redirectPath=${pathname}`,
+          `https://blood-for-bd.vercel.app/auth?redirectPath=${pathname}`,
           request.url
         )
       );
